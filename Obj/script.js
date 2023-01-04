677690127a377624ce8b1eb465952f785b3d2fff
Henil999 <97076815+Henil999@users.noreply.github.com>
2023-01-04 14:12:16 +0530

    Object

---
+++ Obj/script.js
@@ -0,0 +1,32 @@
+let BMWobj={
+    model:"BMW",
+    price:100000,
+    color:"white",
+    engine:"v8",
+    year:2023,
+};
+let Audiobj={
+    model:"Audi",
+    price:200000,
+    color:"red",
+    engine:"v8",
+    year:2021,
+};
+let Benzobj={
+    model:"Mercedes",
+    price:300000,
+    color:"red",
+    engine:"v8",
+    year:2022,
+}
+function BMW(BMWobj){
+    console.log(this.BMWobj)
+}
+
+function Audi(){
+    console.log("Audi Clicked........")
+}
+
+function Benz(){
+    console.log("Benz Clicked........")
+}
